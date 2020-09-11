6eea0d4a9d14216c540f75fcde2e6fb5f9a3a129
Alexander Sheiko <alexander.sheiko@mgid.com>
2020-09-11 17:46:56 +0300

    Add gitlab releases

---
+++ pkg/gitlab/gitlab.go
@@ -0,0 +1,40 @@
+package gitlab
+
+import (
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/n0madic/site2rss"
+)
+
+func gitlabReleaseRSS() (string, error) {
+	return site2rss.NewFeed("https://about.gitlab.com/releases/categories/releases/", "GitLab releases").
+		GetItemsFromQuery(".article", func(doc *site2rss.Selection, opts *site2rss.FindOnPage) *site2rss.Item {
+			url := "https://about.gitlab.com" + doc.Find("a.more").First().AttrOr("href", "")
+			author := doc.Find("div.author > strong > a").First().Text()
+			title := strings.TrimSpace(doc.Find("div.overlay > h3").First().Text())
+			created, _ := time.Parse("Jan 2, 2006", strings.TrimSpace(doc.Find("div.date").First().Text()))
+			desc, _ := doc.Find(".summary").First().Html()
+			desc = strings.TrimSpace(desc)
+			return &site2rss.Item{
+				Title:       title,
+				Author:      &site2rss.Author{Name: author},
+				Link:        &site2rss.Link{Href: url},
+				Id:          url,
+				Description: desc,
+				Created:     created,
+			}
+		}).GetRSS()
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	rss, err := gitlabReleaseRSS()
+	if err == nil {
+		w.Header().Set("Content-Type", "application/xml")
+		w.Write([]byte(rss))
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+	}
+}
--- router.go
+++ router.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 
 	"github.com/n0madic/site2rss-proxy/pkg/flibusta"
+	"github.com/n0madic/site2rss-proxy/pkg/gitlab"
 	"github.com/n0madic/site2rss-proxy/pkg/pikabu"
 
 	"github.com/gorilla/mux"
@@ -15,6 +16,7 @@ func NewRouter(prefix string) *mux.Router {
 	prefix = strings.TrimSuffix(prefix, "/")
 	mux := mux.NewRouter()
 	mux.HandleFunc(prefix+"/flibusta/genre/{genre}", flibusta.Handler)
+	mux.HandleFunc(prefix+"/gitlab/blog/releases", gitlab.Handler)
 	mux.HandleFunc(prefix+"/pikabu/{location}/{name}", pikabu.Handler)
 
 	return mux
