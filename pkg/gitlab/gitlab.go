78d47e020d5d4e5e447321587753f914f3074346
Alexander Sheiko <alexander.sheiko@mgid.com>
2021-01-12 13:35:46 +0200

    Fix GitLab releases

--- pkg/gitlab/gitlab.go
+++ pkg/gitlab/gitlab.go
@@ -2,30 +2,22 @@ package gitlab
 
 import (
 	"net/http"
-	"strings"
-	"time"
 
 	"github.com/n0madic/site2rss"
 )
 
 func gitlabReleaseRSS() (string, error) {
 	return site2rss.NewFeed("https://about.gitlab.com/releases/categories/releases/", "GitLab releases").
-		GetItemsFromQuery(".blog-card", func(doc *site2rss.Selection, opts *site2rss.FindOnPage) *site2rss.Item {
-			url := "https://about.gitlab.com" + doc.Find(".blog-card-title").First().AttrOr("href", "")
-			author := doc.Find(".blog-card-author > a").First().Text()
-			title := strings.TrimSpace(doc.Find(".blog-card-title > h3").First().Text())
-			created, _ := time.Parse("Jan 2, 2006", strings.TrimSpace(doc.Find(".blog-card-date").First().Text()))
-			desc, _ := doc.Find(".blog-card-excerpt").First().Html()
-			desc = strings.TrimSpace(desc)
-			return &site2rss.Item{
-				Title:       title,
-				Author:      &site2rss.Author{Name: author},
-				Link:        &site2rss.Link{Href: url},
-				Id:          url,
-				Description: desc,
-				Created:     created,
-			}
-		}).GetRSS()
+		GetLinks(".blog-hero-title, .blog-card-title").
+		SetParseOptions(&site2rss.FindOnPage{
+			Title:       ".blog.article > div.wrapper.body > h1",
+			Author:      "span.author > a:nth-child(1)",
+			Date:        "span.date",
+			DateFormat:  "Jan 2, 2006",
+			Description: ".content",
+		}).
+		GetItemsFromLinks(site2rss.ParseItem).
+		GetRSS()
 }
 
 // Handler HTTP
