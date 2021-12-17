f3243cf23730525bcff1e755e750e6d8bbf2cf1f
Alexander Sheiko <alexander.sheiko@mgid.com>
2021-12-17 12:58:16 +0200

    Add Golang blog feed

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/n0madic/site2rss-proxy
 go 1.13
 
 require (
-	github.com/aws/aws-lambda-go v1.27.0
+	github.com/aws/aws-lambda-go v1.27.1
 	github.com/awslabs/aws-lambda-go-api-proxy v0.11.0
 	github.com/gorilla/mux v1.8.0
-	github.com/n0madic/site2rss v0.0.0-20211207220335-a8f026c9b8e6
+	github.com/n0madic/site2rss v0.0.0-20211217105240-26a8be54e2bd
 )
---
+++ pkg/golang/blog.go
@@ -0,0 +1,32 @@
+package golang
+
+import (
+	"net/http"
+
+	"github.com/n0madic/site2rss"
+)
+
+func golangBlogRSS() (string, error) {
+	return site2rss.NewFeed("https://golang-blog.blogspot.com/", "Golang blog").
+		SetParseOptions(&site2rss.FindOnPage{
+			Title:       ".post-title",
+			Date:        ".date-header > span",
+			DateFormat:  "Monday, 2 January 2006 г.",
+			Description: ".post-body",
+			URL:         ".post-title > a",
+		}).
+		GetItemsFromQuery(".date-outer", site2rss.ParseQuery).
+		GetRSS()
+}
+
+// Handler HTTP
+func Handler(w http.ResponseWriter, r *http.Request) {
+	rss, err := golangBlogRSS()
+	if err == nil {
+		w.Header().Set("Content-Type", "application/xml")
+		w.Write([]byte(rss))
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+	}
+}
